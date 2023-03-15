1807240cabad809a20c805ff7a0e7838c249780b
Johnny Fang <100670788+johnnyf-hcp@users.noreply.github.com>
2023-03-15 11:05:21 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -84,7 +84,7 @@ resource "aws_instance" "hashicups-docker-server" {
   tags = {
     Name = "${var.prefix}-${var.environment}-hashicups-app"
     Owner = "${var.prefix}"
-    Purpose = "Field Dem"
+    Purpose = "Field Demo - Johnny Fang"
     Environment = "${var.environment}"
   }
 }
